7362bccccdf1c423de836d6b257170330620a499
WIN_NT\x16j050 <x16j050@chiba-fjb.ac.jp>
2017-06-02 11:56:55 +0900

    移動

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,43 @@ namespace game
 {
     public partial class Form1 : Form
     {
+        int speed = 10;
+        int moveX;
+        int moveY;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveY = -speed;
+            moveX = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            moveY = +speed;
+            moveX = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            moveX = -speed;
+            moveY = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            moveX = +speed;
+            moveY = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += moveX;
+            pictureBox1.Top += moveY;
         }
     }
 }
